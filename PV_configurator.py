ad28c4f98b5955cdb62be1b0cd33094488955537
hosseinghandi <ghandih22@gmail.com>
2026-04-02 18:05:10 +0200

    Refactor PV_configurator.py for clarity and structure

--- PV_configurator.py
+++ PV_configurator.py
@@ -1,29 +1,23 @@
 #solar_panel= {0: ["Mono-Si" ,1.75]}
 import ghpythonlib.component as ghc
 
-                
 #decription of the inputs 
 ghenv.Component.Params.Input[0].Description = "A rating your module is given to determine how much sunlight it absorbs and converted into usable electricity as percentage"
 ghenv.Component.Params.Input[1].Description = 'Module lenfth e.g., 1.1m (unite meter)'
 ghenv.Component.Params.Input[2].Description = 'Module width e.g., 1.1m (unite meter)'
 ghenv.Component.Params.Input[3].Description = 'The Nominal Maximum Power of PV systems is given in W by manufacturer.'
 
-               
 ghenv.Component.Params.Output[0].Description = 'The defined PV system data'
                 
 ghenv.Component.Name = 'PV_configurator'
 ghenv.Component.NickName = 'PV_configurator'
-ghenv.Component.Message = '23.5.2025'
 ghenv.Component.Category = 'PV_panel'
 ghenv.Component.Description = 'This component make an user defined PV system.'
                 
-
-
 PV_system = []
 
 def main():
  
-
     try:
         if (module_efficiency == None and module_capacity == None):
             raise ValueError("It is necesary to provide either module-efficency or module-capacity.")
@@ -47,9 +41,6 @@ def main():
                 int(modul_cap_calculated), int(module_cap)))
         
         PV_system.append((round(module_eff,2), module_length, module_width, module_cap))
-
-
-
 
     except ValueError as e:
         ghc.add_warning(str(e))    
