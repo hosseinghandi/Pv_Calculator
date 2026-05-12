7b879e652edefd7a7c7c2e979db11866d2dbc06c
hossein Ghandi <ghandih22@gmail.com>
2026-05-12 12:11:59 +0200

    Add files via upload

---
+++ Rhino_8/PV_Initializer_v1_p3.py
@@ -0,0 +1,236 @@
+"""Grasshopper Script Instance"""
+import System
+import Rhino
+import Grasshopper
+
+import rhinoscriptsyntax as rs
+import System
+import Rhino
+import Grasshopper
+import json
+import rhinoscriptsyntax as rs
+import ghpythonlib.components as gh
+import ghpythonlib.component as ghc
+import rhinoscriptsyntax as rh 
+from Rhino.Geometry import Brep
+import math 
+import csv
+from datetime import datetime, timedelta
+from collections import namedtuple
+from ghpythonlib.treehelpers import list_to_tree
+
+
+class MyComponent(Grasshopper.Kernel.GH_ScriptInstance):
+    def RunScript(self,
+            _buildings: System.Collections.Generic.List[Rhino.Geometry.GeometryBase],
+            _EPW_file,
+            angle_: float,
+            monthly_period):
+        # Role of the component:
+        # A) Gets building geometry and picks out the roof of the building to prepare a calculation surface on which Ladybug should simulate.
+        # B) Gets EPW weather file and extracts necessary data for simulation and corresponding calculations.
+        # C-D) If the user provided the inclination of the panels, calculates the Solar Elevation Angle to calculate the panel distances.
+        #      Moreover, takes the period preference of the user to create a proper data list,
+        #      based on which the data is passed to other components.
+        
+        # Description of the inputs
+        ghenv.Component.Params.Input[0].Description = (
+        'Rhino Breps representing the buildings for which PV panel output will be calculated. '
+        'Please ensure the input is flattened and that you are providing valid geometry '
+        '(must be a solid, valid polySurface).'
+        )
+        ghenv.Component.Params.Input[1].Description = (
+        'An EPW file path as a string pointing to the file location on your system.'
+        )
+        ghenv.Component.Params.Input[2].Description = (
+        'Inclination of the PV panels in degrees. If no inclination is provided, the algorithm '
+        'assumes a horizontal installation strategy.\n'
+        'Default value: 0 (horizontal)'
+        )
+        ghenv.Component.Params.Input[3].Description = (
+        'Set to "True" to enable monthly analysis. '
+        'If not provided, the entire year will be considered by Ladybug.'
+        )
+        
+        # Description of the Outputs
+        ghenv.Component.Params.Output[0].Description = (
+        'The selected roof surface(s) for which PV panel output will be calculated.'
+        )
+        ghenv.Component.Params.Output[1].Description = (
+        'This output should be connected to the PV_output_calculator component.'
+        )
+        
+        # Description of component
+        ghenv.Component.Name = 'PV_Initializer'
+        ghenv.Component.NickName = 'PV_Initializer'
+        ghenv.Component.Category = 'PV_Panel'
+        ghenv.Component.Description = (
+        'Extracts building roof geometry and EPW weather data to prepare '
+        'the necessary inputs for PV energy simulation via PV_output_calculator.'
+        )
+        def cal_elevation(latitude : float, longitude : float, timezone : float) -> float:
+            try:
+                if all([latitude is not None, longitude is not None, timezone is not None]):
+                    #The row distance in PV fields is customarily determined by the shadow length on 21 December at solar noon
+                    #For more data check https://doi.org/10.3390/en14133850 
+                    date_time = datetime(2024, 12, 21, 12, 0, 0) 
+                    lat_rad = math.radians(latitude)
+                    N = date_time.timetuple().tm_yday
+
+                    # Solar Declination (δ)
+                    delta = 23.45 * math.sin(math.radians((360 / 365) * (N - 81)))
+                    # Hour Angle (γ)
+                    
+                    lstm = 15 * float(timezone)
+                    TC1 = 4*(longitude-lstm)
+                    B = math.radians((360 / 365) * (N - 81))
+                    EOT = 9.87 * math.sin(2 * B) - 7.53 * math.cos(B) - 1.5 * math.sin(B)
+                    TC = TC1 + EOT
+                    LT = 12 
+                    LST = LT + (TC / 60)
+                    gamma = 15 * (LST - 12)
+
+                    # Solar Elevation Angle (α)
+                    return (
+                        math.degrees(math.asin(
+                            math.sin(lat_rad) * math.sin(math.radians(delta)) +
+                            math.cos(lat_rad) * math.cos(math.radians(delta)) * math.cos(math.radians(gamma))
+                        )))
+                else: raise ValueError("Latitude, longitude, or timezone is missing.")
+            except Exception as e:
+                raise RuntimeError(f"Solar elevation calculation failed: {e}")
+
+        def epw_open(epw_path : str) -> list[str]: 
+            if not epw_path:
+                raise ValueError("No EPW file path provided.")
+            try :
+                with open(epw_path, 'r') as epw:
+                    reader = csv.reader(epw)
+                    location = next(reader)
+                    location_data =(location [1] , location [6] , location [7])
+                    latitude = float(location [6])
+                    longitude = float(location [7])
+                    timezone = float(location[8])
+                    
+                    # Skip the first 8 lines of EPW file since they are not necessary data
+                    for _ in range(7):
+                        next(reader)
+                        
+                    model_year = []
+                    for row in reader:
+                        model_year.append(str(row[0]))
+                    #take necessary info for location, lat and long
+                    model_year_bounds = str(gh.Bounds(model_year)).split(",")
+                    return ((
+                        f"1. Environmental and Geographical:\n\n"
+                        f"Location: {location_data[0]}\n"
+                        f"Latitude: {float(location_data[1]):.2f}\n"
+                        f"Longitude: {float(location_data[2])}\n"
+                        f"Weather Data Source: EPW file based on model years "
+                        f"{model_year_bounds[0]}-{model_year_bounds[1]}\n\n"
+                    ),(latitude, longitude, timezone)
+                    )
+            except FileNotFoundError :
+                raise FileNotFoundError(f"EPW file not found at path: {epw_path}")
+            except Exception as e:
+                raise RuntimeError(f"[epw_open] Something went wrong: {e}")
+
+        def addTiltSurface(roofs : list[Brep] , ang : float) -> Brep :
+            try:
+                if (roofs):
+                    if len(roofs) == 1:
+                        cen = gh.ConstructPoint(0,0,max(gh.Deconstruct(gh.Area(roofs)[1])) + 20) 
+                    else:
+                        cen = gh.ConstructPoint(0,0,max(gh.Deconstruct(gh.Area(roofs)[1])[2]) + 20)
+
+                    plane = gh.Rectangle(cen, 2, 2, 0)[0]
+                    ax = gh.Explode(plane, True)[0][0]
+                    tiltPlane = gh.RotateAxis(plane, gh.Radians(ang), ax)[0]
+                    return tiltPlane
+                else: raise ValueError("The roof input is missed")
+            except Exception as e:
+                raise RuntimeError(f"Something went wrong: {e}")
+
+        def roof_picker(buildings : list[Brep], ang : float) -> list[Brep]:
+            roofs=[]
+            try:
+                if len(buildings) > 0:
+                    for building in buildings:
+                        dec = gh.DeconstructBrep(building)
+                        for i in range(len(dec[0])):
+                            cent = gh.Area(dec[0][i])[1]
+                            norm = gh.EvaluateSurface((dec[0][i]) ,cent)[1]
+                            if norm[2]==1:
+                                if cent[2] > 0 :
+                                    roofs.append(dec[0][i])
+                            if norm[2]==-1 and cent[2] > 0:
+                                roofs.append(dec[0][i])
+                else: raise ValueError("Building geometry could not be deconstructed!")
+                
+                # If inclined, add a single inclined surface to calculate Global Tilted Irradiance (GTI).
+                # This method is used to save simulation time, considering the trade-off for inaccuracy which can be 
+                # acceptable for early stage of design.
+                if ang > 0:
+                    tilt_panel = gh.BoundarySurfaces(addTiltSurface(roofs, ang))
+                    roofs.append(tilt_panel)
+                    print (tilt_panel)
+                return roofs
+            except Exception as e:
+                raise RuntimeError(f"Something went wrong while handling buildings: {e}")
+
+        def data_maker(period: bool) -> list[int]:
+            required_hours = [] 
+            if period: 
+                monthly_hours = (744 , 672, 744 , 720, 744, 720, 744, 744, 720, 744, 720, 744 )
+                prev_index = 0
+                for month in range(12) : 
+                    required_hours.append(list(range(prev_index, monthly_hours[month] + prev_index)))
+                    prev_index += monthly_hours[month]
+                required_hours = list_to_tree(required_hours)
+            return required_hours
+
+        roofs, pv_data_pack, period_hoys = None, None, None
+        try:
+            
+            # handel default values 
+            ang = 0 if angle_ == None else angle_
+            period = False if monthly_period is None else monthly_period
+            
+            # handel primary data 
+            locationInfo, locationData = epw_open(_EPW_file)
+            latitude, longitude,timezone = locationData
+            solar_elevation_angle = cal_elevation(latitude, longitude,timezone) if ang > 0 else 0
+            
+            # output
+            roofs = roof_picker(_buildings,ang)
+            pv_data_pack = json.dumps({
+                "locationInfo": locationInfo,
+                "spacing" : (solar_elevation_angle, ang),
+                "period" : f"{ 'yearly' if not period else 'monthly'}"
+            })
+            period_hoys = None if not period else data_maker(period)
+
+        except Exception as e:
+            ghc.add_warning(f"{e}")
+        return roofs, pv_data_pack, period_hoys
+
+
+    # Preview overrides 
+    def get_ClippingBox(self):
+        return Rhino.Geometry.BoundingBox.Empty
+
+    def DrawViewportWires(self, args):
+        pass
+
+    def DrawViewportMeshes(self, args):
+        pass
+
+    # Solve overrides 
+    def BeforeRunScript(self):
+        pass
+
+    def AfterRunScript(self):
+        pass
+
+
+
---
+++ Rhino_8/PV_estimator_v1_p3.py
@@ -0,0 +1,338 @@
+"""Grasshopper Script Instance"""
+import System
+import Rhino
+import Grasshopper
+
+import rhinoscriptsyntax as rs
+import ghpythonlib.components as gh
+import ghpythonlib.component as ghc
+from collections import namedtuple
+import math 
+import json
+
+
+class MyComponent(Grasshopper.Kernel.GH_ScriptInstance):
+    def RunScript(self,
+            _roofs: System.Collections.Generic.List[Rhino.Geometry.GeometryBase],
+            _points: System.Collections.Generic.List[Rhino.Geometry.Point3d],
+            _result: System.Collections.Generic.List[float],
+            _pv_data_pack: str,
+            _generate_single_building_: bool,
+            reduction_coefficient: float,
+            PV_system: str):
+        # Role of the component:
+        # This component plays an important role to wrap all the data and calculate the PV output assumption
+        # the workflow here is :
+        # Get the result and point on which the simulation has been conducted -> 
+        # evalute the roof area and all user inputs to provide required data for the formula of Pv Out calculation ->
+        # once you calculate it be ready for any scenario changes ->
+        # Provide data based on the user request : ->
+        #A) General information 
+        #B) Monthly information 
+        #C) annual information 
+        #d) single building Production information
+        
+        
+        # Description of the inputs
+        ghenv.Component.Params.Input[0].Description = 'The geometry of the PV panels'
+        ghenv.Component.Params.Input[1].Description = 'The grid of points that is be used to perform the incident radiation analysis.'
+        ghenv.Component.Params.Input[2].Description = 'The result(s) from the simulation is needed'
+        ghenv.Component.Params.Input[3].Description = 'The pv-data package by (PV_Data_Provider) should be connected here'
+        ghenv.Component.Params.Input[4].Description = 'Generating Production for each building (result in KWh/year)'
+        ghenv.Component.Params.Input[5].Description = 'The reduction coefficient presents a percentage of the roof that can be used to install PV system considering other obstructs such as AC system device. The default value is set to 50 percentage.' 
+        ''
+        ghenv.Component.Params.Input[6].Description = "You can define your PV system using the 'PV_system_definer' component."
+        "By default, the system is configured with the following values:\nEfficiency: 20%\nPanel Dimensions: 1.75m (length) × 1m (width)\nPV Capacity: 350 Wp"
+        
+        # Description of the Outputs      
+        ghenv.Component.Params.Output[0].Description = 'The result of PV panel installation based on the requested data on KWh/year.'
+        
+        # Description of component            
+        ghenv.Component.Name = 'PV_estimator'
+        ghenv.Component.NickName = 'PV_estimator'
+        ghenv.Component.Message = '23.5.2025'
+        ghenv.Component.Category = 'PV_panel'
+        ghenv.Component.Description = 'This component provides final information for user.'
+
+        
+        def find_distance(pv_pack ,pv_system):
+            # find the distance within PV panels and total area needed for each panel based on the inclination
+            tilted_panel_length = 0
+            panel_area_required = 0
+            distance_within_PV = 0
+            # data extraction 
+            inclination = pv_pack["spacing"][1]
+            spacing = pv_pack["spacing"][0]
+            length = pv_system["length"]
+            width = pv_system["width"]
+
+            if inclination:
+                # check https://www.mdpi.com/1996-1073/14/13/3850 for calcualtion formula information
+                distance_within_PV = length*math.sin(math.radians(inclination))/math.tan(math.radians(spacing))
+                tilted_panel_length= length * math.cos(math.radians(inclination))
+                panel_area_required = (distance_within_PV + tilted_panel_length) * (width)
+            else:
+                distance_within_PV = 0
+                panel_area_required = length * width  
+            return tilted_panel_length, panel_area_required, distance_within_PV
+
+
+        def cal_PV_installation(roofs ,panel_area_required ,distance_within_PV,pv_pack ,pv_system ):
+            roofs = list(roofs)
+            roofs = roofs[:-1] if distance_within_PV > 0 else roofs
+            roofs_area = []
+            possible_roofs_installation = []
+            possible_panels_installation = [] 
+            # pv effective area refers to actual area of the pv panel not the distance it occupies 
+            # while pv occupied area refers to total area needed for each single pv including spacing if applied
+            pv_effective_area = []
+            pv_occupied_area = []
+            # if there is just one roof make it list to be able to iterate over it
+            # if gh.ListLength(roofs) 
+            if gh.ListLength(roofs) == 1:
+                roofs = [roofs]
+
+            for i, roof in enumerate(roofs):
+                width, length = pv_system["width"], pv_system["length"]
+                roof_area = gh.Area(roof)[0]
+                roofs_area.append(roof_area)
+                possible_roofs_installation.append(roof_area * (reduction_coe))
+                possible_panels_installation.append(int((possible_roofs_installation[i] / panel_area_required))) 
+                pv_effective_area.append(possible_panels_installation[i] * (length * width)) 
+                pv_occupied_area.append(possible_panels_installation[i] * panel_area_required)
+
+            return roofs_area, possible_roofs_installation, possible_panels_installation, pv_effective_area, pv_occupied_area
+
+
+        def create_PV_layout(roofs,pv_pack ,pv_system):
+            pv_pack = json.loads(pv_pack)
+            (tilted_panel_length, panel_area_required, distance_within_PV) = find_distance(pv_pack,pv_system)
+
+            (roofs_area, 
+            possible_roofs_installation, 
+            possible_panels_installation,
+            pv_effective_area, pv_occupied_area) = cal_PV_installation(roofs,panel_area_required, distance_within_PV, pv_pack, pv_system)
+
+
+
+
+        def get_Losses(res):
+            result_average = gh.Average(res)
+            return 1-((abs(max(res) - result_average)/((max(res)+ result_average)/2)) + 0.145)
+
+
+        def cal_Production(res, pv_effective_area, eff, maxVal) :
+            losses = get_Losses(res)
+            if isinstance(pv_effective_area, list):
+                eff_Area = sum(pv_effective_area)
+            else:
+                eff_Area = pv_effective_area
+            
+            return round((eff_Area * float(losses) * float(maxVal) * float(eff)),2)
+
+
+        def  info_provider_general_yearly(res,pv_effective_area , eff, isinclined):
+            if isinclined :
+                res = list(res)
+                GTI = max(res)
+                GHI= res[:-1]
+                return cal_Production(GHI,pv_effective_area, eff, GTI)
+            else: return cal_Production(res,pv_effective_area, eff, max(res))
+            
+
+        def structure_monthly_result(res):
+            res = list(res)
+            result_corrected_tree = []
+            monthly_count = int(len(res) / 12)
+            for i in range(12):
+                start = i * monthly_count
+                end = start + monthly_count
+                result_corrected_tree.append(res[start:end])
+            return result_corrected_tree
+
+
+        def info_provider_general_monthly(res,pv_effective_area , eff , isinclined):
+            result_corrected_tree = structure_monthly_result(res)
+            #in kwh/m2
+            monthly_Production = []
+            for monthly_ghi in result_corrected_tree:
+                maxValue = max(monthly_ghi)
+                if isinclined:
+                    maxValue = monthly_ghi[-1]
+                    monthly_ghi = monthly_ghi[: -1]
+                monthly_Production.append(cal_Production(monthly_ghi,pv_effective_area, eff, maxValue))
+            return monthly_Production
+
+        def handelProducationValue(prod):
+            if prod >= 1000000:
+                return f"{prod / 1000000:.2f} GWh/year"
+
+            elif prod >= 1000:
+                return f"{prod / 1000:.2f} MWh/year"
+
+            else:
+                return f"{prod:.2f} kWh/year"
+
+
+        def build_summary(
+            roofs_area ,
+            possible_roofs_installation ,
+            pv_effective_area, 
+            possible_panels_installation,
+            reduction_coe,
+            pv_pack, 
+            PV_system, 
+            maxValue, 
+            losses, 
+            production, 
+            isInclined,
+            distance_within_PV
+            ):
+            
+            if (isInclined):
+                solarPower = f"The maximum Global Tilted Irradiance:{maxValue:.2f} kWh/m²/year\n"
+                spacing = f"PanelTilt Angle: {pv_pack['spacing'][1]}°\nRow Spacing: {distance_within_PV:.2f} m \n" 
+            else:
+                solarPower = f"The maximum Global Horizontal Irradiance:{maxValue:.2f} kWh/m²/year\n"
+
+            return (
+                f"Photovoltaic (PV) System Performance Summary\n\n"
+                f"Note: The presented results are based on average system loss assumptions applied uniformly across all evaluated buildings to streamline the assessment process. "
+                f"Consequently, these figures may slightly overestimate real-world performance. For more accurate projections, individual building assessments are recommended.\n\n"
+                f"{pv_pack['locationInfo']}"
+                f"2.PV System Installation Details:\n\n"
+                f"Total Rooftop area:{sum(roofs_area):.2f}m².\n"
+                f"Reduction coefficient:{reduction_coe * 100}% of total roof area.\n"
+                f"Rooftop area utilized :{sum(possible_roofs_installation):.2f}m².\n"
+                f"Effective area of the panels: {sum(pv_effective_area):.2f} m²\n"
+                f"Number of Installed Panels: {sum(possible_panels_installation)} units\n"
+                f"{'' if not isInclined else spacing}"
+                f"Panel Dimensions: {PV_system['length']} m × {PV_system['width']} m\n"
+                f"Total Installed Capacity: {(sum(possible_panels_installation) * (PV_system['cap'] / 1000)):.2f} kWp\n\n"
+                f"3. PV Panel teachnical Specifications:\n"
+                f"Panel Capacity: {int(PV_system['cap'])} Wp\n"
+                f"Efficiency: {int(PV_system['eff'] * 100)}%\n\n"
+                f"Energy production:\n"
+                f"{solarPower}"
+                f"System losses: {(1 - losses) * 100:.2f}% (average value applied across all buildings).\n"
+                f"Estimated Annual Energy Production: {handelProducationValue(production)}.\n"
+            )
+
+
+        def info_provider_single_building(
+            roofs, 
+            pts , 
+            res , 
+            pv_effective_area, 
+            eff, 
+            isInclined):
+            Production = []
+            # Handle inclined case 
+            if isInclined:
+                # take out placeholder panel for getting GTI
+                res = res[: - 1]
+                pts = pts[: - 1]
+                roofs = roofs[: - 1]
+            # Global max irradiance
+            GHI = max(res)
+            for i, roof in enumerate(roofs):
+                # Extrude roof to capture points
+                geo = gh.Extrude(roof, gh.UnitZ(1))
+                values = []
+                for j, pt in enumerate(pts):
+                    if gh.PointInBrep(geo, pt, False) == 1:
+                        values.append(res[j])
+
+                if not values:
+                    Production.append(0)
+                else:
+                    Production.append(
+                        cal_Production(values, pv_effective_area[i], eff, GHI)
+                    )
+            return Production
+
+
+        def checkRequiredInputs(roofs, pts, res, pv_pack):
+            errors = []
+            message = ""
+            if gh.ListLength(res) <= 1 :
+                errors.append("Result list is empty or failed to collect.")
+            if not roofs or gh.ListLength(roofs) == 0 :
+                errors.append("Roof geometry is missing.")
+            if not pts or gh.ListLength(pts) < 1:
+                errors.append("Analysis points are missing.")
+            if not pv_pack :
+                errors.append("PV data pack from 'PV_initializer' is required.")
+            if reduction_coefficient > 1 or reduction_coefficient == 0 :
+                errors.append(f"reduction_coefficient is a value from 0 to 1 which presents but got {reduction_coefficient}.")
+            if errors:
+                numbered_errors = [ (f"{i+1}-{err}") for i, err in enumerate(errors) ]
+                message = (
+                        "\nRequired Inputs Missing:\n"
+                        + "\n".join(numbered_errors)
+                        + "\n\nPlease check the following inputs' error."
+                    )
+                raise RuntimeError(message)
+                
+        information = None
+        try:
+            checkRequiredInputs(_roofs, _points, _result, _pv_data_pack)
+            # default values 
+            reduction_coe = 0.5 if reduction_coefficient is None else reduction_coefficient  
+            # Loading Json and default assignment
+            PV_system = {"eff":0.21,"length": 1.564 , "width" : 1.144, "cap": 360} if PV_system is None else json.loads(PV_system)
+            pv_pack = json.loads(_pv_data_pack)
+            single_pro = False if _generate_single_building_ is None else _generate_single_building_
+
+            (tilted_panel_length, 
+            panel_area_required, 
+            distance_within_PV) = find_distance(pv_pack,PV_system)
+
+            (roofs_area, possible_roofs_installation, possible_panels_installation,
+            pv_effective_area, pv_occupied_area) = cal_PV_installation(_roofs,panel_area_required, distance_within_PV, pv_pack, PV_system)
+
+            isInclined = True if pv_pack["spacing"][1] != 0 else False
+            isMonthly = pv_pack["period"] == "monthly" 
+            if single_pro:
+                if isMonthly : raise RuntimeError("Monthly production calculation is not available for a single building.")
+                information = info_provider_single_building(_roofs, _points, _result, pv_effective_area, PV_system["eff"], isInclined)
+                
+            if not isMonthly and not single_pro:
+                production = info_provider_general_yearly(_result,pv_effective_area, PV_system["eff"],isInclined)
+                information = build_summary(
+                    roofs_area,
+                    possible_roofs_installation,pv_effective_area, possible_panels_installation,
+                    reduction_coe,
+                    pv_pack, 
+                    PV_system, 
+                    max(_result), 
+                    get_Losses(_result), 
+                    production, 
+                    isInclined,
+                    distance_within_PV)
+            elif (isMonthly and not single_pro): 
+                information = info_provider_general_monthly(_result,pv_effective_area, PV_system["eff"],isInclined)
+
+
+        except Exception as e:
+            ghc.add_warning(f"{e}")
+                            
+        return information
+
+
+    # Preview overrides 
+    def get_ClippingBox(self):
+        return Rhino.Geometry.BoundingBox.Empty
+
+    def DrawViewportWires(self, args):
+        pass
+
+    def DrawViewportMeshes(self, args):
+        pass
+
+    # Solve overrides 
+    def BeforeRunScript(self):
+        pass
+
+    def AfterRunScript(self):
+        pass
